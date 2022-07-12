9fe8d4a6d51f68af6b46b34a4ec906150af7c0a2
EGorio8 <egor420@outlook.com>
2022-07-13 00:22:53 +0300

    react №1

---
+++ src1/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+function Footer(){
+    return(
+        <div className="Footer-site-footer">
+            <b>React site</b>
+            <p>jhkjhjhjhibvjhvhjvjhvhjv</p>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ taran/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './components/Header';
+import Title from './components/Title';
+import Map from './components/Map';
+
+const App = () =>{
+  return(
+    <div className='container'>
+      <Header menu={["Москва", "Питер"]}/>
+      <div className="content">
+        <Title/>
+        <Map/>
+
+      </div>
+    </div>
+    )
+  }
+
+export default App
